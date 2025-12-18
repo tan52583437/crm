ffe8f64ff51e10b7c004d71c5816a80162754ad7
tan52583437 <52583437@qq.com>
2025-12-18 11:21:04 +0800

    Use environment variable for server port

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
